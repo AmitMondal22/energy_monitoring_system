0288f63102aec5005eee43d32097b21e75e97737
Amit Mondal <amit.ndk3@gmail.com>
2024-03-29 13:05:24 +0530

    add git ignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# fastapi_temp
-# energy_monitoring_system
--- controllers/device_to_server/EnergyController.py
+++
@@ -1,5 +0,0 @@
-def get_energy_data(data):
-    try:
-      return data  
-    except Exception as e:
-        raise ValueError("Could not fetch data")
