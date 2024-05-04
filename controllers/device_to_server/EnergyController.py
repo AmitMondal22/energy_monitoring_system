2a6182921451f3d41447ae42800b8494fecd8e7f
amitmondal22 <amit.ndk3@gmail.com>
2024-05-04 18:14:32 +0530

    test

--- controllers/device_to_server/EnergyController.py
+++ controllers/device_to_server/EnergyController.py
@@ -67,7 +67,7 @@ async def send_last_energy_data(client_id, device_id, device):
             # background_tasks.add_task(AlertLibrary.send_alert, client_id, device_id, device, lastdata)
             
             
-            await manager.send_personal_message("EMS",client_id, device_id, device, json.dumps(lastdata, cls=DecimalEncoder))
+            await manager.send_personal_message("EMS",client_id, device_id, device, "hello")
             
             print("lastdata last energy data>>>>>>>>>>/////////",json.dumps(lastdata, cls=DecimalEncoder))
             return json.dumps(lastdata, cls=DecimalEncoder)
