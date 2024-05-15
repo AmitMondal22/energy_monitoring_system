ad384e7d969014b5aed8b50504d63aed84f38f71
amitmondal22 <amit.ndk3@gmail.com>
2024-05-15 16:58:35 +0530

    add send ws

--- controllers/device_to_server/EnergyController.py
+++ controllers/device_to_server/EnergyController.py
@@ -164,7 +164,7 @@ async def send_last_energy_data(client_id, device_id, device):
             # print("Last data",lastdata_weekdata)
             
             background_tasks.add_task(AlertLibrary.send_alert, client_id, device_id, device, json.dumps(lastdata, cls=DecimalEncoder))
-
+            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
             await sennd_ws_message("EMS",client_id, device_id, device, json.dumps(lastdata, cls=DecimalEncoder))
             # return json.dumps(lastdata, cls=DecimalEncoder)
         except Exception as e:
